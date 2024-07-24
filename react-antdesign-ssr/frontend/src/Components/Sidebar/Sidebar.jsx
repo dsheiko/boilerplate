eb77d050d80165b52a7877b4e72e0c351f9f9386
Dmitry Sheiko <dmitry@crytek.com>
2024-07-24 15:06:48 +0200

    refactor(react-antdesign-ssr):  Sidebar to functional style, Antd Menu upgrated, hydration warnings fixed

--- react-antdesign-ssr/frontend/src/Components/Sidebar/Sidebar.jsx
+++ react-antdesign-ssr/frontend/src/Components/Sidebar/Sidebar.jsx
@@ -1,44 +1,22 @@
 import React from "react";
-import PropTypes from "prop-types";
 import ErrorBoundary from "../ErrorBoundary";
 import { ProjectOutlined } from '@ant-design/icons';
-import { Layout, Menu, Icon } from "antd";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import actions from "~/Actions";
+import { Layout, Menu } from "antd";
+import { useSelector } from "react-redux";
 
-const { Content, Sider } = Layout,
-      { SubMenu } = Menu;
+const { Sider } = Layout;
 
+export default function Sidebar() {
 
-// Mapping state to the props
-const mapStateToProps = ( state ) => ({ store: state }),
-      // Mapping actions to the props
-      mapDispatchToProps = ( dispatch ) => ({
-        actions: bindActionCreators( actions, dispatch )
-      });
+     const menu = useSelector( ( state ) => state.app.menu ),
+           items = menu.map(  project => ({ label: project, key: project, icon: <ProjectOutlined /> }) );
 
-// Using store connect as a decorator
-@connect( mapStateToProps, mapDispatchToProps )      
-export default class Sidebar extends React.Component {
-
-  static displayName = "Sidebar";  
-
-  render() {
-    const { store } = this.props;
-    return (< ErrorBoundary>
+    return (<ErrorBoundary>
     <Sider width={200} style={{ background: '#fff' }}>
         <Menu
           mode="inline"
           style={{ height: '100%', borderRight: 0 }}
-        >
-
-        { store.app.menu.map( ( project, inx ) => (<Menu.Item key={ inx }>
-          <span><ProjectOutlined />{ project }</span>
-        </Menu.Item>)) }
-
-        </Menu>
+          items={ items } />
       </Sider>
     </ ErrorBoundary>);
-  }
 };
